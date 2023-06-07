e697025ff88c44e6b48f83b4c7f79f24d215f6bf
davidsaltacc <133795772+davidsaltacc@users.noreply.github.com>
2023-06-07 11:51:13 +0200

    Update load.js

--- load.js
+++ load.js
@@ -2,5 +2,5 @@ scriptElement = document.createElement("script");
 
 document.body.appendChild(scriptElement); 
 
-requestObject = new Request("URL");
+requestObject = new Request("https://raw.githubusercontent.com/davidsaltacc/cookieclicker-improved/main/main.js");
 fetch(requestObject).then((response) => response.text()).then((text) => {scriptElement.innerHTML = text});
