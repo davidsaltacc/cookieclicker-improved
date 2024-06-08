d5badf8dc9e1e989dbc84c480edfad6a7520edc5
david <133795772+davidsaltacc@users.noreply.github.com>
2024-06-08 17:38:11 +0200

    better load

--- load.js
+++ load.js
@@ -1,6 +1,1 @@
-scriptElement = document.createElement("script");
-
-document.body.appendChild(scriptElement); 
-
-requestObject = new Request("https://raw.githubusercontent.com/davidsaltacc/cookieclicker-improved/main/main.js");
-fetch(requestObject).then((response) => response.text()).then((text) => {scriptElement.innerHTML = text});
+fetch("https://raw.githubusercontent.com/davidsaltacc/cookieclicker-improved/main/main.js").then((r) => r.text()).then((c) => { var scriptElement = document.createElement("script"); scriptElement.innerHTML = c; document.body.appendChild(scriptElement); });
